ec78cd47f852fb461fb2b4e08b5d4a7b2abc893e
Aditya Vaja <wolverine.av@gmail.com>
2016-07-08 12:46:17 -0700

    remove ivs from p-only deployments

--- bosi/scripts/startup.sh
+++ bosi/scripts/startup.sh
@@ -7,7 +7,5 @@ rpm -ivh --force /root/python-networking-bigswitch-${bsnstacklib_version}-1.el7.
 rpm -ivh --force /root/openstack-neutron-bigswitch-agent-${bsnstacklib_version}-1.el7.centos.noarch.rpm
 rpm -ivh --force /root/openstack-neutron-bigswitch-lldp-${bsnstacklib_version}-1.el7.centos.noarch.rpm
 rpm -ivh --force /root/python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm
-rpm -ivh --force /root/ivs-${ivs_version}-1.el7.centos.x86_64.rpm
-rpm -ivh --force /root/ivs-debuginfo-${ivs_version}-1.el7.centos.x86_64.rpm
 systemctl enable neutron-bsn-lldp.service
 systemctl restart neutron-bsn-lldp.service
