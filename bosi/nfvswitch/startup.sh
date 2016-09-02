c0bed4fa1490aa4178c158c7e931376d43fba532
Sarath Kumar <sarath.kutty@bigswitch.com>
2016-09-02 13:19:02 -0700

    nfvswitch: add customize and starup scripts for rhel packaging

---
+++ bosi/nfvswitch/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum remove -y openstack-neutron-bigswitch-agent
+yum remove -y openstack-neutron-bigswitch-lldp
+yum remove -y python-networking-bigswitch
+rpm -ivh --force /root/python-networking-bigswitch-${bsnstacklib_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/openstack-neutron-bigswitch-agent-${bsnstacklib_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/openstack-neutron-bigswitch-lldp-${bsnstacklib_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/nfvswitch-${nfvswitch_version}-1.el7.centos.x86_64.rpm
+rpm -ivh --force /root/nfvswitch-debuginfo-${nfvswitch_version}-1.el7.centos.x86_64.rpm
+systemctl enable neutron-bsn-lldp.service
+systemctl restart neutron-bsn-lldp.service
