b88d618102812a90f330fc176489aae555c80d0d
Aditya Vaja <wolverine.av@gmail.com>
2017-04-09 15:52:20 -0700

    update yaml files for RHOSP 10
    updated/added the following:
     - bigswitch-config-<p/pv>.yaml
     - network-environment.yaml
     - first-boot.yaml
    
    moved existing master to mitaka and liberty points to mitaka.
    newton and ocata to point to master with the updates.

---
+++ bosi/rhosp_resources/origin/stable/mitaka/ivs/customize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export LIBGUESTFS_BACKEND=direct
+
+image_dir="/home/stack/images"
+
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload python-networking-bigswitch-${networking_bigswitch_version}-1.el7.centos.noarch.rpm:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload openstack-neutron-bigswitch-lldp-${networking_bigswitch_version}-1.el7.centos.noarch.rpm:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload openstack-neutron-bigswitch-agent-${networking_bigswitch_version}-1.el7.centos.noarch.rpm:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}.el7.centos.x86_64.rpm:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm:/root/
+
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --firstboot startup.sh
+
---
+++ bosi/rhosp_resources/origin/stable/mitaka/nfvswitch/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ISOL_CPU="2-55"
+HUGEPAGES="12288"
+
+yum remove -y openstack-neutron-bigswitch-agent
+yum remove -y openstack-neutron-bigswitch-lldp
+yum remove -y python-networking-bigswitch
+
+rpm -ivh --force /root/python-networking-bigswitch-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/openstack-neutron-bigswitch-agent-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/openstack-neutron-bigswitch-lldp-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivh --force /root/python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm
+
+systemctl enable neutron-bsn-lldp.service
+systemctl restart neutron-bsn-lldp.service
+
+if [[ $(hostname) == *compute* ]]; then
+    rpm -ivh --force /root/ivs-${ivs_version}.el7.centos.x86_64.rpm
+    rpm -ivh --force /root/ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm
+    rpm -ivh --force /root/nfvswitch-${nfvswitch_version}.el7.centos.x86_64.rpm
+    rpm -ivh --force /root/nfvswitch-debuginfo-${nfvswitch_version}.el7.centos.x86_64.rpm
+
+    grubby --update-kernel=ALL --args="isolcpus=$ISOL_CPU nohz_full=$ISOL_CPU hugepages=$HUGEPAGES iommu=pt intel_iommu=on"
+    reboot
+fi
