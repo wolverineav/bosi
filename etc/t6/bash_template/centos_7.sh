b0fe5ff4f22e1eeed3d348e75bfe7f96c4f77354
Aditya Vaja <wolverine.av@gmail.com>
2017-04-12 14:05:20 -0700

    correct the path for install_pkg
     -  and make dependency installation optional

--- etc/t6/bash_template/centos_7.sh
+++ etc/t6/bash_template/centos_7.sh
@@ -10,6 +10,7 @@ fuel_cluster_id=%(fuel_cluster_id)s
 openstack_release=%(openstack_release)s
 skip_ivs_version_check=%(skip_ivs_version_check)s
 pip_proxy=%(pip_proxy)s
+offline_dir=%(dst_dir)s/offline
 
 
 controller() {
@@ -168,7 +169,7 @@ compute() {
 
 install_pkg() {
     pkg=$1
-    cd %(dst_dir)s/upgrade
+    cd $offline_dir
     tar -xzf $pkg
     dir=${pkg::-7}
     cd $dir
@@ -184,18 +185,21 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# prepare dependencies
-yum install -y wget
+# in case of offline installation, these dependencies are expected to be pre-installed
+if [[ ! -d $offline_dir ]]; then
+    # prepare dependencies
+    yum install -y wget
 
-wget -r --no-parent --no-directories --timestamping --accept 'epel-release-7-*.rpm' 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
-rpm -iUvh epel-release-7-*.rpm
-rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm
-yum groupinstall -y 'Development Tools'
-yum install -y python-devel puppet python-pip wget libffi-devel openssl-devel
-easy_install pip
-pip install --upgrade funcsigs
-puppet module install --force puppetlabs-inifile
-puppet module install --force puppetlabs-stdlib
+    wget -r --no-parent --no-directories --timestamping --accept 'epel-release-7-*.rpm' 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/'
+    rpm -iUvh epel-release-7-*.rpm
+    rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm
+    yum groupinstall -y 'Development Tools'
+    yum install -y python-devel puppet python-pip wget libffi-devel openssl-devel
+    easy_install pip
+    pip install --upgrade funcsigs
+    puppet module install --force puppetlabs-inifile
+    puppet module install --force puppetlabs-stdlib
+fi
 
 # install bsnstacklib
 if [[ $install_bsnstacklib == true ]]; then
@@ -203,7 +207,6 @@ if [[ $install_bsnstacklib == true ]]; then
     pip uninstall -y bsnstacklib
     sleep 2
 
-    offline_dir=%(dst_dir)s/offline
     if [[ -d $offline_dir ]]; then
         # install from offline package dir if available
         PKGS=$offline_dir/*
