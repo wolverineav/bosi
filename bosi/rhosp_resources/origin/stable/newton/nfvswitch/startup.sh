e92c2454e169158d75306c9f8043ae4675303479
Aditya Prakash Vaja <wolverine.av@gmail.com>
2017-11-21 08:35:21 -0800

    OSP-131: BOSI Ocata readiness
     - moves all old templates to separate stable/newton directory
     - everything under master now has only Ocata and upwards

---
+++ bosi/rhosp_resources/origin/stable/newton/nfvswitch/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum remove -y openstack-neutron-bigswitch-agent
+yum remove -y openstack-neutron-bigswitch-lldp
+yum remove -y python-networking-bigswitch
+rpm -ivhU --force /root/python-networking-bigswitch-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivhU --force /root/openstack-neutron-bigswitch-agent-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivhU --force /root/openstack-neutron-bigswitch-lldp-${networking_bigswitch_version}-1.el7.centos.noarch.rpm
+rpm -ivhU --force /root/python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm
+rpm -ivhU --force /root/ivs-${ivs_version}.el7.centos.x86_64.rpm
+rpm -ivhU --force /root/ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm
+systemctl enable neutron-bsn-lldp.service
+systemctl restart neutron-bsn-lldp.service
