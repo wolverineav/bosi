e290a7255578f7d990a07c3090208e59854a86ab
Aditya Prakash Vaja <wolverine.av@gmail.com>
2018-09-22 03:32:20 +0530

    workaround for IVS restart issue
     - os-net-config stopped restarts after config file change
     - this causes IVS to not send LLDP since it doesn't have the
       refreshed interface list
     - this reverses the patch temporarily, until proper fix goes to
       os-net-config

--- bosi/rhosp_resources/master/ivs/customize.sh
+++ bosi/rhosp_resources/master/ivs/customize.sh
@@ -19,7 +19,9 @@ export LIBGUESTFS_BACKEND=direct
 image_dir="/home/stack/images"
 
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload neutron-bsn-lldp-${lldp_version}-1.el7.centos.noarch.rpm:/root/
-virt-customize -a ${image_dir}/overcloud-full.qcow2 --firstboot startup.sh
 # enabled for P+V mode
 #virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}.el7.centos.x86_64.rpm:/root/
 #virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm:/root/
+
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload impl_ifcfg.py:/root/
+virt-customize -a ${image_dir}/overcloud-full.qcow2 --firstboot startup.sh
