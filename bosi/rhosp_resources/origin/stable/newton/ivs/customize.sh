d8e9b0b27b679135ee663948fc9ae4e3a5603562
Aditya Prakash Vaja <wolverine.av@gmail.com>
2018-10-15 22:56:37 +0530

    update customize.sh to define ivs_version and use it locally

--- bosi/rhosp_resources/origin/stable/newton/ivs/customize.sh
+++ bosi/rhosp_resources/origin/stable/newton/ivs/customize.sh
@@ -8,8 +8,11 @@ virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload python-networking-b
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload openstack-neutron-bigswitch-lldp-${networking_bigswitch_version}-1.el7.centos.noarch.rpm:/root/
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload openstack-neutron-bigswitch-agent-${networking_bigswitch_version}-1.el7.centos.noarch.rpm:/root/
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm:/root/
-virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}.el7.centos.x86_64.rpm:/root/
-virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm:/root/
+# enabled for P+V mode
+#ivs_version="TODO set_IVS_version_here_if_deploying_PV_mode"
+#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}.el7.centos.x86_64.rpm:/root/
+#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm:/root/
+#sed -i -e "s/\${ivs_version}/$IVS_VERSION_REVISION/" ./startup.sh
 
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --firstboot startup.sh
 
