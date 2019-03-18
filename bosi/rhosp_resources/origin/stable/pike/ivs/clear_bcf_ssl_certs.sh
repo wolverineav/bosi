189f156e4b0e1bc0b3be53990782ea0f1fda22a6
Aditya Prakash Vaja <wolverine.av@gmail.com>
2019-03-18 14:03:17 -0700

    add helper script to clear bcf ssl certs when forced to do so

---
+++ bosi/rhosp_resources/origin/stable/pike/ivs/clear_bcf_ssl_certs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux
+
+# Make sure only root can run this script
+if [ "$(id -u)" != "0" ]; then
+   echo -e "Please run as root"
+   exit 1
+fi
+
+CERTS_DIR=`docker inspect neutron_api | jq ".[0].GraphDriver.Data.UpperDir" | tr -d '"'`
+
+echo "deleting certs from "$CERTS_DIR
+rm -rf $CERTS_DIR/var/lib/neutron/host_certs/*.pem
+rm -rf $CERTS_DIR/var/lib/neutron/ca_certs/*.pem
+rm -rf $CERTS_DIR/var/lib/neutron/combined/*.pem
+
+docker restart neutron_api
