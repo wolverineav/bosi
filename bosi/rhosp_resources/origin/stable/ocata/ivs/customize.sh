0df0c8b31545112c05e1c2c1c194debe9441b1cd
Aditya Prakash Vaja <wolverine.av@gmail.com>
2019-03-18 11:20:15 -0700

    change to queens no propogated through other releases

--- bosi/rhosp_resources/origin/stable/ocata/ivs/customize.sh
+++ bosi/rhosp_resources/origin/stable/ocata/ivs/customize.sh
@@ -10,9 +10,9 @@ virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload openstack-neutron-b
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload python-horizon-bsn-${horizon_bsn_version}-1.el7.centos.noarch.rpm:/root/
 # enabled for P+V mode
 #ivs_version="TODO set_IVS_version_here_if_deploying_PV_mode"
-#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}.el7.centos.x86_64.rpm:/root/
-#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}.el7.centos.x86_64.rpm:/root/
-#sed -i -e "s/\${ivs_version}/$IVS_VERSION_REVISION/" ./startup.sh
+#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-${ivs_version}-1.el7.centos.x86_64.rpm:/root/
+#virt-customize -a ${image_dir}/overcloud-full.qcow2 --upload ivs-debuginfo-${ivs_version}-1.el7.centos.x86_64.rpm:/root/
+#sed -i -e "s/\${ivs_version}/$ivs_version/" ./startup.sh
 
 virt-customize -a ${image_dir}/overcloud-full.qcow2 --firstboot startup.sh
 
